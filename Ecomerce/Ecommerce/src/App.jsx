37fda941374266f38116b1c52923cc2e20815a7d
MayanthaS <sapumalmayantha@gmail.com>
2025-11-09 01:11:16 +0530

    split a header in to a component and use Link

--- Ecomerce/Ecommerce/src/App.jsx
+++ Ecomerce/Ecommerce/src/App.jsx
@@ -1,6 +1,8 @@
 import {Routes,Route} from 'react-router';
 import { Homepage } from './pages/HomePage'
 import { Checkoutpage} from './pages/Checkoutpage';
+import { Orderspage } from './pages/Orderspage';
+import { Trackingpage } from './pages/Trackingpage';
 import './App.css'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <Routes>
       <Route index element={<Homepage />}/>
       <Route path='checkout' element={<Checkoutpage />}/>
+      <Route path='orders' element={<Orderspage/>} />
+      <Route path=''/>
       
       
     </Routes>
