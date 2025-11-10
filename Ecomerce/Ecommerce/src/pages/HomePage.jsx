cd785cfef783c74c6ab3a30c53107d7866da00b1
MayanthaS <sapumalmayantha@gmail.com>
2025-11-10 11:34:08 +0530

    fetch data into the backend

--- Ecomerce/Ecommerce/src/pages/HomePage.jsx
+++ Ecomerce/Ecommerce/src/pages/HomePage.jsx
@@ -5,6 +5,12 @@ import { products } from "../starting-code/data/products";
 import "./HomePage.css";
 
 export function Homepage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) =>{
+     return response.json()
+    }).then((data) =>{
+          console.log(data);
+      });
   return (
     <>
       <title>Ecommerce</title>
